a372a3e79f028b8a96523eb72b054dc609f746f8
dmpadhiyar0705@outlook.com <dmpadhiyar0705@outlook.com>
2026-05-30 15:23:15 +0530

    Add project files.

---
+++ 04ContactFormSystem/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+using _04ContactFormSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Runtime;
+
+namespace _04ContactFormSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult ContactPage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ContactPage(ContactModel model)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            ViewBag.Message= $"Form submitted successfully! Thank you {model.Name}";
+
+            ViewBag.Name = model.Name;
+            ViewBag.Email = model.Email;
+            ViewBag.MessageContent = model.Message;
+
+            ModelState.Clear(); // Clear the form after successful submission
+            return View(new ContactModel());
+        }
+    }
+}
